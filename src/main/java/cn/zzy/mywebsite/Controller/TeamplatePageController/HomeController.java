8e50b8c9c4965b36ffbd77833c58d78b673c8d7e
zzy <1156846412@qq.com>
2018-05-09 20:44:47 +0800

    给首页的时钟添加按钮效果,点击后跳转到主页,暂时将主页映射到博客页面

--- src/main/java/cn/zzy/mywebsite/Controller/TeamplatePageController/HomeController.java
+++ src/main/java/cn/zzy/mywebsite/Controller/TeamplatePageController/HomeController.java
@@ -1,6 +1,8 @@
 package cn.zzy.mywebsite.Controller.TeamplatePageController;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "/Home")
 public class HomeController {
 
+    @Autowired
+    private BlogController blogController;
+
     @RequestMapping(method = RequestMethod.GET)
-    public String HomePage()
+    public String HomePage(Model model)
     {
-        return "Home";
+        return blogController.BlogPage(model);
     }
 }
