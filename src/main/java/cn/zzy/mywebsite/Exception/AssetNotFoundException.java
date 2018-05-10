3a8ad8dd864156503ab2fb577e8450c9d0f0e277
zzy <1156846412@qq.com>
2018-05-10 18:31:39 +0800

    添加资源未找到异常类

---
+++ src/main/java/cn/zzy/mywebsite/Exception/AssetNotFoundException.java
@@ -0,0 +1,22 @@
+package cn.zzy.mywebsite.Exception;
+
+public class AssetNotFoundException extends RuntimeException {
+    public AssetNotFoundException() {
+    }
+
+    public AssetNotFoundException(String message) {
+        super(message);
+    }
+
+    public AssetNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AssetNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public AssetNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
