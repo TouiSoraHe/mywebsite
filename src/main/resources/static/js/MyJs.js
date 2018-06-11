778b37aa7482a3c3267f7bf4772e707b2e001337
zhangzhanyi <zhangzhanyi@i-focusing.com>
2018-06-11 17:55:32 +0800

    删除页面中的模态框,统一由js来产生模态框

--- src/main/resources/static/js/MyJs.js
+++ src/main/resources/static/js/MyJs.js
@@ -8,45 +8,95 @@ function FormatDateTime(time, regex) {
     var m = ("0" + myDate.getMinutes()).slice(-2);
     var s = ("0" + myDate.getSeconds()).slice(-2);
     var mi = ("00" + myDate.getMilliseconds()).slice(-3);
-    return regex.replace("yyyy",year).replace("yy",year%100).replace("MM",month).replace("dd",day)
-        .replace("hh",h>12?h%12:h).replace("HH",h).replace("mm",m).replace("ss",s).replace("mi",mi);
+    return regex.replace("yyyy", year).replace("yy", year % 100).replace("MM", month).replace("dd", day)
+        .replace("hh", h > 12 ? h % 12 : h).replace("HH", h).replace("mm", m).replace("ss", s).replace("mi", mi);
 };
 
-NProgress.configure({ minimum: 0.4 });
+$('body').append('<script src="/nprogress/js/nprogress.js"></script>');
+NProgress.configure({minimum: 0.4});
 
-function RequestAsyn(url,method,successEvent,errorContent,errorModal,form) {
+function RequestAsyn(url, method, successEvent, form) {
     NProgress.done();
     NProgress.start();
     $.ajax({
-        url:url,
-        dataType:"json",
-        type:method,
-        processData:false,
+        url: url,
+        dataType: "json",
+        type: method,
+        processData: false,
         contentType: false,
-        success:function (data) {
-            if(data.code!=200){
-                fail(data.code,data.message,errorContent,errorModal);
+        success: function (data) {
+            if (data.code != 200) {
+                fail(data.code, data.message);
             }
-            else
-            {
+            else if (successEvent != null && successEvent instanceof Function) {
                 successEvent(data.data);
             }
         },
-        error:function (jqXHR, textStatus) {fail(jqXHR.status,textStatus,errorContent,errorModal);},
-        data:form,
-        complete:function () {
+        error: function (jqXHR, textStatus) {
+            fail(jqXHR.status, textStatus);
+        },
+        data: form,
+        complete: function () {
             NProgress.done();
         }
     });
 }
 
-function fail(code,message,errorContent,errorModal) {
-    if(errorContent!=null && errorModal!=null){
-        errorContent.html("错误代码:"+code+((message==null || message=="")?"":"<br>错误信息:"+message));
-        errorModal.modal('show');
+function fail(code, message) {
+    SimpleModal("服务器开了一点小差", "<div>错误代码:" + code + ((message == null || message == "") ? "" : "<br>错误信息:" + message) + "</div>");
+}
+
+/**
+ * 简单模态框
+ * @param title 模态框的标题,String类型
+ * @param content 模态框的内容
+ * @param okEvent 点击确认按钮时的事件,参数:content(jquery对象)
+ * @constructor
+ */
+function SimpleModal(title, content, okEvent) {
+    var modalID = 'modalID' + Date.now();
+    var titleID = 'titleID' + Date.now();
+    var modal = $(
+        '<div class="modal fade" id="modalID" tabindex="-1" role="dialog" aria-labelledby="titleID" aria-hidden="true">\n' +
+        '    <div class="modal-dialog">\n' +
+        '        <div class="modal-content">\n' +
+        '            <div class="modal-header">\n' +
+        '                <button type="button" class="close" data-dismiss="modal"\n' +
+        '                        aria-hidden="true">×\n' +
+        '                </button>\n' +
+        '                <h4 class="modal-title" id="titleID">\n' +
+        '                    标题\n' +
+        '                </h4>\n' +
+        '            </div>\n' +
+        '            <div class="modal-body">\n' +
+        '            </div>\n' +
+        '            <div class="modal-footer">\n' +
+        '                <button type="button" class="btn btn-default"\n' +
+        '                        data-dismiss="modal">确认\n' +
+        '                </button>\n' +
+        '            </div>\n' +
+        '        </div>\n' +
+        '    </div>\n' +
+        '</div>');
+    $('body').append(modal);
+    modal.attr('id', modalID);
+    modal.attr('aria-labelledby', titleID);
+    var titleDom = modal.find('.modal-title');
+    titleDom.attr('id', titleID);
+    var contentDom = modal.find('.modal-body');
+    var buttonDom = modal.find('.modal-footer > button');
+    titleDom.text(title);
+    if (!(content instanceof jQuery)) {
+        content = $(content);
     }
-    else{
-        console.log("错误代码:"+code);
-        console.log("错误信息:"+message);
-    }
+    contentDom.append(content);
+    buttonDom.on('click', function () {
+        if (okEvent != null && okEvent instanceof Function) {
+            okEvent(content);
+        }
+    });
+    modal.on('hidden.bs.modal', function () {
+        modal.remove();
+    });
+    modal.modal('show');
 }
